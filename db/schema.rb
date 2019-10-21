bdeb7f6454b1fa84d13caf81dc5931e79cecf573
Tish Chungoora <tishal82@gmail.com>
2019-10-21 12:00:49 +0100

    Added ActiveRecord migration and model for School

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_104238) do
+ActiveRecord::Schema.define(version: 2019_10_21_105823) do
+
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
