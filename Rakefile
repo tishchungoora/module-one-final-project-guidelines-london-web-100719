683ea74053a26cfa7bafaaef01a939591f17a61b
Tish Chungoora <tishal82@gmail.com>
2019-10-21 14:52:14 +0100

    Improved seed file to generate necessary dummy data

--- Rakefile
+++ Rakefile
@@ -6,3 +6,10 @@ task :console do
   ActiveRecord::Base.logger = Logger.new(STDOUT)
   Pry.start
 end
+
+namespace :db do
+  desc 'seed the database with some dummy data'
+  task :seed do
+    require_relative './db/seeds.rb'
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,13 @@
 require_relative '../config/environment.rb'
 
-User.create(username: Faker::NatoPhoneticAlphabet.code_word, password: Faker::Team.creature, logged_in: Faker::Boolean.boolean(true_ratio: 0.5))
-User.create(username: Faker::NatoPhoneticAlphabet.code_word, password: Faker::Team.creature, logged_in: Faker::Boolean.boolean(true_ratio: 0.5))
-User.create(username: Faker::NatoPhoneticAlphabet.code_word, password: Faker::Team.creature, logged_in: Faker::Boolean.boolean(true_ratio: 0.5))
-User.create(username: Faker::NatoPhoneticAlphabet.code_word, password: Faker::Team.creature, logged_in: Faker::Boolean.boolean(true_ratio: 0.5))
-User.create(username: Faker::NatoPhoneticAlphabet.code_word, password: Faker::Team.creature, logged_in: Faker::Boolean.boolean(true_ratio: 0.5))
-User.create(username: Faker::NatoPhoneticAlphabet.code_word, password: Faker::Team.creature, logged_in: Faker::Boolean.boolean(true_ratio: 0.5))
-User.create(username: Faker::NatoPhoneticAlphabet.code_word, password: Faker::Team.creature, logged_in: Faker::Boolean.boolean(true_ratio: 0.5))
-User.create(username: Faker::NatoPhoneticAlphabet.code_word, password: Faker::Team.creature, logged_in: Faker::Boolean.boolean(true_ratio: 0.5))
-User.create(username: Faker::NatoPhoneticAlphabet.code_word, password: Faker::Team.creature, logged_in: Faker::Boolean.boolean(true_ratio: 0.5))
+User.destroy_all
+School.destroy_all
+Course.destroy_all
+Review.destroy_all
+
+10.times do |count|
+    User.create(username: "user#{count + 1}", password: Faker::Team.creature, logged_in: Faker::Boolean.boolean(true_ratio: 0.5))
+end
 
 schools = ["Flatiron","Makers","General"]
 School.create(name: schools[0])
@@ -21,14 +20,8 @@ Course.create(topic: courses[1], is_online: Faker::Boolean.boolean(true_ratio: 0
 Course.create(topic: courses[2], is_online: Faker::Boolean.boolean(true_ratio: 0.5), is_in_person: Faker::Boolean.boolean(true_ratio: 0.5))
 Course.create(topic: courses[3], is_online: Faker::Boolean.boolean(true_ratio: 0.5), is_in_person: Faker::Boolean.boolean(true_ratio: 0.5))
 
-Review.create(school_id: School.all.sample.id, course_id: Course.all.sample.id, user_id: User.all.sample.id, rating: rand(1..5), content: Faker::Hacker.say_something_smart)
-Review.create(school_id: School.all.sample.id, course_id: Course.all.sample.id, user_id: User.all.sample.id, rating: rand(1..5), content: Faker::Hacker.say_something_smart)
-Review.create(school_id: School.all.sample.id, course_id: Course.all.sample.id, user_id: User.all.sample.id, rating: rand(1..5), content: Faker::Hacker.say_something_smart)
-Review.create(school_id: School.all.sample.id, course_id: Course.all.sample.id, user_id: User.all.sample.id, rating: rand(1..5), content: Faker::Hacker.say_something_smart)
-Review.create(school_id: School.all.sample.id, course_id: Course.all.sample.id, user_id: User.all.sample.id, rating: rand(1..5), content: Faker::Hacker.say_something_smart)
-Review.create(school_id: School.all.sample.id, course_id: Course.all.sample.id, user_id: User.all.sample.id, rating: rand(1..5), content: Faker::Hacker.say_something_smart)
-Review.create(school_id: School.all.sample.id, course_id: Course.all.sample.id, user_id: User.all.sample.id, rating: rand(1..5), content: Faker::Hacker.say_something_smart)
-Review.create(school_id: School.all.sample.id, course_id: Course.all.sample.id, user_id: User.all.sample.id, rating: rand(1..5), content: Faker::Hacker.say_something_smart)
-Review.create(school_id: School.all.sample.id, course_id: Course.all.sample.id, user_id: User.all.sample.id, rating: rand(1..5), content: Faker::Hacker.say_something_smart)
-Review.create(school_id: School.all.sample.id, course_id: Course.all.sample.id, user_id: User.all.sample.id, rating: rand(1..5), content: Faker::Hacker.say_something_smart)
+10.times do |count|
+    Review.create(school_id: School.all.sample.id, course_id: Course.all.sample.id, user_id: User.all.sample.id, rating: rand(1..5), content: Faker::Hacker.say_something_smart)
+end
+
 
