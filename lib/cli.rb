2272eff9ec2d5c310b680a135c936721dc61f192
Tish Chungoora <tishal82@gmail.com>
2019-10-24 09:15:28 +0100

    Moved former cli class files into deprecated folder and performing initial refactoring for manage own review scenarios

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,18 +4,43 @@ class Cli
     end
 
     def login(username)
-        @user = User.find_by_username(username)
+        @user = User.find_or_create_by_username(username)
     end
 
-    def run
-        input = greet
-        login(input)
-        menu
-    end
-
-
-    def greet
-        puts "hellp, whats your username?"
+    def manage_menu
+        puts "Please select an option from below:"
+        puts "- update : update the content of a review"
+        puts "- delete : delete a review"
+        puts "- exit : return to user menu"
         gets.chomp
     end
+
+    def update_review
+        # user reviews
+        # select specific review
+        puts "Now type your new review:"
+        new_content = gets.chomp
+        review.update_review_content(new_content)
+        puts "Your review has been submitted. Thank you!"
+    end
+
+    def delete_review
+        # user reviews
+        # select specific review
+        # destroy
+    end
+
+    def run_manage_reviews
+        choice = manage_menu
+        case choice
+        when "update"
+            puts "Run update method"
+        when "delete"
+            puts "Run delete method"
+        when "exit"
+            manage_menu
+        end
+        manage_menu
+    end
+
 end
--- lib/models/review.rb
+++ lib/models/review.rb
@@ -2,4 +2,8 @@ class Review < ActiveRecord::Base
     belongs_to :school
     belongs_to :course
     belongs_to :user
+
+    def update_review_content(content)
+        self.update(content: content)
+    end
 end
